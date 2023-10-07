3c91abd9d5aeaa15e94aea73b6b6e8c767ade03a
chaentopia <cce523@hanmail.net>
2023-10-07 14:35:12 +0900

    [Chore] Project Setting

---
+++ DO_SOPT_33_Seminar/DO_SOPT_33_Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DO_SOPT_33_Seminar
+//
+//  Created by 정채은 on 10/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
