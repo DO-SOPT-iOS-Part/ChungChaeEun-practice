9b2fe724359f26acd8f0717ccfef193a088e8d02
chaentopia <cce523@hanmail.net>
2023-10-08 23:14:10 +0900

    [Feat] Navigation 화면 전환 구현

---
+++ DO_SOPT_33_Seminar/DO_SOPT_33_Seminar/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  DO_SOPT_33_Seminar
+//
+//  Created by 정채은 on 10/7/23.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+    }
+    
+}
--- DO_SOPT_33_Seminar/DO_SOPT_33_Seminar/ViewController.swift
+++ DO_SOPT_33_Seminar/DO_SOPT_33_Seminar/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var idText: String = ""
+    private var passwordText: String = ""
+    
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+//    @IBAction func idTextFieldChanged(_ sender: Any) {
+//        guard let textField = sender as? UITextField else {return}
+//        if let idText = textField.text {
+//            self.idText = idText
+//        }
+//        print("아이디 입력 완료")
+//    }
+    
+    @IBAction func idTextFieldChanged(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let idText = textField.text {
+            self.idText = idText
+        }
+        print("아이디 입력 완료")
+    }
+    
+    @IBAction func loginTextFieldChanged(_ sender: Any) {
+        guard let textField = sender as? UITextField else {return}
+        if let passwordText = textField.text {
+            self.passwordText = passwordText
+        }
+        print("비밀번호 입력 완료")
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        print("\(idText)\n\(passwordText)")
+        
+        pushToResultVC()
+    }
+    
+    func pushToResultVC() {
+        guard let resultViewController = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
+        self.navigationController?.pushViewController(resultViewController, animated: true)
+    }
 }
 
