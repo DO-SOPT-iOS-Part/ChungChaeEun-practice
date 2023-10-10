e03e0c924cf7ce1584751087aeed9ec9dfda6727
chaentopia <cce523@hanmail.net>
2023-10-10 15:29:03 +0900

    [Feat] Protocol 구현

---
+++ DO_SOPT_33_Seminar/DO_SOPT_33_Seminar/GetDataProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  GetDataProtocol.swift
+//  DO_SOPT_33_Seminar
+//
+//  Created by 정채은 on 10/10/23.
+//
+
+import Foundation
+
+protocol GetDataProtocol {
+    func getLoginData(email: String, password: String)
+}
