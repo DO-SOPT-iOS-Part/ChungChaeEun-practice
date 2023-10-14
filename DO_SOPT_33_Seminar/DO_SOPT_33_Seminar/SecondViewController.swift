27966f4c438525e387757bdcda3d48952f490433
chaentopia <cce523@hanmail.net>
2023-10-14 14:35:31 +0900

    [Feat] SceneDelegate 설정

---
+++ DO_SOPT_33_Seminar/DO_SOPT_33_Seminar/SecondViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.swift
+//  DO_SOPT_33_Seminar
+//
+//  Created by 정채은 on 10/14/23.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
